fe18d6c23ba922bff8417a9bd0bca65d9b391e6d
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-06 22:47:18 +0300

    i dun goofed, reverting a bit

--- server/config/express.js
+++ server/config/express.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var SessionStore = require('session-mongoose')(express);
 var passport = require('passport')
 
 module.exports = function(app, config) {
@@ -14,14 +13,9 @@ module.exports = function(app, config) {
 	app.use(bodyParser.urlencoded({
 	  extended: true
 	}));
-	app.use(session({
-		store: new SessionStore({
-			url: 'mongodb://localhost/library',
-			interval: 1200000
-		}),
-		secret: 'sloth power',
-		cookie: { maxAge: 1200000}
-	}));
+	app.use(session({secret: 'sloth power',
+					 saveUninitialized:true,
+					 resave: true}));
 	app.use(passport.initialize());
 	app.use(passport.session());
 	app.use(express.static(config.rootPath + '/public'));
