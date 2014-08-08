86eb733794565342dfe8178fc42e1205d284f3e3
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-08 16:57:41 +0300

    removed express-session and added cookie session in its place

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var http = require('http');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
+var session = require('cookie-session');
 var passport = require('passport')
 
 module.exports = function(app, config) {
@@ -13,9 +14,13 @@ module.exports = function(app, config) {
 	app.use(bodyParser.urlencoded({
 	  extended: true
 	}));
-	app.use(session({secret: 'sloth power',
-					 saveUninitialized:true,
-					 resave: true}));
+	// app.use(session({secret: 'sloth power',
+	// 				 saveUninitialized:true,
+	// 				 resave: true}));
+	app.use(session({
+		keys: ['sporo', 'loro'],
+		maxage: 1200000
+	}))
 	app.use(passport.initialize());
 	app.use(passport.session());
 	app.use(express.static(config.rootPath + '/public'));
