84aa907ffa57dc96a27320322eec9aa91410f86a
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-20 22:33:30 +0300

    mongolab now added as an addon to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -6,13 +6,12 @@ module.exports = {
 	developement: {
 		rootPath: rootPath,
 		db: 'mongodb://localhost/library',
-		port: process.env.PORT || 5050
+		port: 5050
 	},
 
 	production: {
 		rootPath: rootPath,
-		db: 'mongodb://admin:rolcaotraci@ds053109.mongolab.com:53109/libraryinventory',
-		port: process.env.PORT || 5050
+		db: process.env.MONGOLAB_URI
 	},
 	
 }
