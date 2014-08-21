d8c9dc524d9a643c55adcd5fc8a9b18696b97804
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-21 15:57:23 +0300

    configuring config.js for new database

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
 
 	production: {
 		rootPath: rootPath,
-		db: process.env.MONGOLAB_URI,
+		db: 'mongodb://heroku_app27722429:tep6vefpquqo2ce9ur3usc3p0n@ds063869.mongolab.com:63869/heroku_app27722429',
 		port: process.env.PORT || 5050
 	},
 	
