eb9de1192bb455b2ef8a95015162c9ff992cdd8b
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-21 16:11:37 +0300

    reverting all changes, i will try not using the addon for now

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
 
 	production: {
 		rootPath: rootPath,
-		db: 'mongodb://heroku_app27722429:tep6vefpquqo2ce9ur3usc3p0n@ds063869.mongolab.com:63869/heroku_app27722429',
+		db: 'mongodb://admin:rolcaotraci@ds053109.mongolab.com:53109/libraryinventory',
 		port: process.env.PORT || 5050
 	},
 	
