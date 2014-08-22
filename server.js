cf74ff69251a676b4a9df77b3636f0cd76d3028a
Angel Simeonov <angel.st.sim@gmail.com>
2014-08-22 15:01:49 +0300

    added http again, now db is on mongolab from an addon on heroku

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var env = process.env.NODE_ENV || 'developement';
 var app = express();
 var config = require('./server/config/config')[env];
 
-require('./server/config/express')(app,config);
+require('./server/config/express')(app,config,http);
 
-require('./server/config/mongoose')(config);
+require('./server/config/mongoose')(config,http);
 
-require('./server/config/passport')();
+require('./server/config/passport')(http);
 
-require('./server/config/routes')(app);
+require('./server/config/routes')(app,http);
 
 app.listen(config.port);
 	console.log("SERVER RUNNING ON PORT: "+ config.port);
