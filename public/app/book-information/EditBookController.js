bb86489db3b4d67e0c51e38955770097d2da83a7
Angel Simeonov <angel.st.sim@gmail.com>
2014-10-04 15:20:09 +0300

    added somewhat buggy sorting for isFormated

--- public/app/book-information/EditBookController.js
+++ public/app/book-information/EditBookController.js
@@ -1,6 +1,7 @@
-libraryApp.controller('EditBookController', function($scope, BookResource, identity){
+libraryApp.controller('EditBookController', function($scope, BookResource, identity, $log){
 	$scope.identity = identity;
 	$scope.books = BookResource.query();
 	$scope.currentPage = 1;
 	$scope.pageSize = 15;
+	$scope.areaStatus = true;
 });
